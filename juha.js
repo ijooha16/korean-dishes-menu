64d07c8ba6efd7904e21fd2e476087ae8e4ded8c
Juha Yoon <ijooha16@gmail.com>
2025-01-06 14:09:59 +0900

    edit design

---
+++ juha.js
@@ -0,0 +1,15 @@
+// const addBtn = document.querySelector('#add_btn');
+
+// window.addEventListener('scroll', () => {
+//     // 화면 높이를 기준으로 동적 조건 설정
+//     const threshold = window.innerHeight * 1.5; // 화면의 50% 높이
+//     const scroll = window.scrollY;
+
+//     if (scroll > threshold) {
+//         addBtn.classList.remove('hide');
+//     } else {
+//         addBtn.classList.add('hide');
+//     }
+// });
+
+console.log('hi');
